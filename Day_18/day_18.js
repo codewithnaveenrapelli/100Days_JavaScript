340ee0485483b4a3162c6da187832c41fc2a04e4
naveenrapelly27 <naveenrapelly27@gmail.com>
2024-04-15 20:23:26 +0530

    Day 18

---
+++ Day_18/day_18.js
@@ -0,0 +1,17 @@
+//? Write a function to check if a character is uppercase or lowercase.
+
+
+//? constrains
+//? The input char will be a single character.
+//? The character can be any printable ASCII character
+//? You can assume that the input will always be a string of length 1.
+
+
+const checkCharacter = (str) => {
+    if (str.charCodeAt(0) >= 65 && str.charCodeAt(0) <= 90) {
+        return true;
+    }
+    return false;
+};
+// console.log(checkCharacter("s")); // false
+console.log(checkCharacter("S")); // true
